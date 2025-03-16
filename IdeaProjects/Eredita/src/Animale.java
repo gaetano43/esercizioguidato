386477225b93ba2243fc3fb6f26fa99cc45175dd
gaetano carpentiere <gaetanocarpentieregaetano71@gmail.com>
2025-03-16 20:57:28 +0100

    animali

---
+++ IdeaProjects/Eredita/src/Animale.java
@@ -0,0 +1,9 @@
+public class Animale {
+    String verso= "un mix di versi";
+
+
+    public void faiIlVerso(String verso){
+        System.out.println("il verso della " + getClass() + " è "+ verso);
+    }
+}
+
