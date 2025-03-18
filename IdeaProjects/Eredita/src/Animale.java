cdb90b0330a45a55e63ba85ba1cb7499b1ed1a9a
gaetano43 <gaetanocarpentiere71@gmail.com>
2025-03-18 10:31:23 +0100

    Delete IdeaProjects/Eredita/src directory

--- IdeaProjects/Eredita/src/Animale.java
+++
@@ -1,9 +0,0 @@
-public class Animale {
-    String verso= "un mix di versi";
-
-
-    public void faiIlVerso(String verso){
-        System.out.println("il verso della " + getClass() + " è "+ verso);
-    }
-}
-
